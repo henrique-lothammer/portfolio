9ffc5390cec6dc268d180f0f23b52b8d08228d1e
Henrique Costa <henrique.costa@argoseguros.com.br>
2021-08-10 16:52:20 -0300

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,31 @@
 import { motion } from "framer-motion";
+import Head from "next/head";
 import GlobalStyle from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <GlobalStyle />
-      <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
-        pageInitial: {
-          opacity: 0
-        },
-        pageAnimate: {
-          opacity: 1
-        },
-      }}>
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
         <Component {...pageProps} />
       </motion.div>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,25 @@
-import Document, {DocumentContext, Html, Head, Main, NextScript} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,9 +28,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
@@ -33,10 +39,16 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"></meta>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0"
+          ></meta>
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap" rel="stylesheet"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
